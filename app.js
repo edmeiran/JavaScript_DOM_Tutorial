9d0d5356294dacb55424e286e479fe7b1243301d
Edgars Meirans <edgarsmeirans@gmail.com>
2021-03-09 16:17:59 +0200

    Video7

--- app.js
+++ app.js
@@ -36,11 +36,20 @@
 
 
 
-const banner = document.querySelector('#page-banner');
+//const banner = document.querySelector('#page-banner');
 
-console.log('#page-banner node type is:', banner.nodeType)
-console.log('#page-banner node name is:', banner.nodeName)
-console.log('#page-banner has child nodes:', banner.hasChildNodes())
+//console.log('#page-banner node type is:', banner.nodeType)
+//console.log('#page-banner node name is:', banner.nodeName)
+//console.log('#page-banner has child nodes:', banner.hasChildNodes())
 
-const clonedBanner = banner.cloneNode(true);
-console.log(clonedBanner);
+//const clonedBanner = banner.cloneNode(true);
+//console.log(clonedBanner);
+
+
+
+const bookList = document.querySelector('#book-list');
+
+console.log('the parent node is:', bookList.parentNode);
+console.log('the parent node is:', bookList.parentElement.parentElement);
+
+console.log(bookList.children);
