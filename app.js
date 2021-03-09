b9db4c2a288cc2329f9f1dfffe9459253c76284c
Edgars Meirans <edgarsmeirans@gmail.com>
2021-03-09 14:48:45 +0200

    Video3

---
+++ app.js
@@ -0,0 +1,9 @@
+var titles = document.getElementsByClassName('title')
+
+//console.log(Array.isArray(titles));
+//console.log(Array.isArray(Array.from(titles)));
+
+Array.from(titles).forEach(function(item){
+    console.log(item);
+})
+    
