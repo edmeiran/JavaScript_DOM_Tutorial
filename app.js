ef12b8455cea8cc987ccde40008b0be6338d4f56
Edgars Meirans <edgarsmeirans@gmail.com>
2021-03-09 15:07:26 +0200

    Video4

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
-var titles = document.getElementsByClassName('title')
+//var titles = document.getElementsByClassName('title')
 
 //console.log(Array.isArray(titles));
 //console.log(Array.isArray(Array.from(titles)));
 
-Array.from(titles).forEach(function(item){
-    console.log(item);
-})
+//Array.from(titles).forEach(function(item){
+//    console.log(item);
+//})
     
+const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+//console.log(wmf)
+
+var books = document.querySelector('#book-list li .name');
+//console.log(books);
+
+books = document.querySelectorAll('#book-list li .name');
+//console.log(books)
+
+Array.from(books).forEach(function(book){
+    console.log(book)
+})
