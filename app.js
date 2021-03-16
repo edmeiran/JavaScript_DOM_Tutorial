e2cfb5f49ffe193a196746fa8c2fd950644f8f42
Edgars Meirans <edgarsmeirans@gmail.com>
2021-03-16 15:41:48 +0200

    video12

--- app.js
+++ app.js
@@ -98,12 +98,25 @@ list.addEventListener('click', function(e){
   }
 });
 
-//add book-list
+//add books
 const addForm = document.forms['add-book'];
 addForm.addEventListener('submit', function(e){
     e.preventDefault();
     const value = addForm.querySelector('input[type="text"]').value;
-    console.log(value);
+   
+//create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+//add content
+deleteBtn.textContent = 'delete';
+bookName.textContent = value;
+
+//apend to document
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
 })
 
 
