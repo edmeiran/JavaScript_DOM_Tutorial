1692eb90318d332a0746a4b635997a6564ac64d8
Edgars Meirans <edgarsmeirans@gmail.com>
2021-03-16 14:26:56 +0200

    Video9

--- app.js
+++ app.js
@@ -56,9 +56,30 @@
 
 
 
-const bookList = document.querySelector('#book-list');
+//const bookList = document.querySelector('#book-list');
 
-console.log('book-list previous sibling is:', bookList.previousSibling)
-console.log('book-list previous element sibling is:', bookList.previousElementSibling)
+//console.log('book-list previous sibling is:', bookList.previousSibling)
+//console.log('book-list previous element sibling is:', bookList.previousElementSibling)
 
-bookList.previousElementSibling.querySelector('p').innerHTML +='<br /> Too cool for everyone else!'
+//bookList.previousElementSibling.querySelector('p').innerHTML +='<br /> Too cool for everyone else!'
+
+
+const listItems = document.querySelectorAll('#book-list ul li');
+
+Array.from(listItems).forEach(function(item){
+  item.addEventListener('click', (e) => {
+
+    const li = e.target.parentElement;
+    console.log('child element to remove:', li);
+    console.log('parent element to remove child from:', li.parentElement);
+    li.parentNode.removeChild(li);
+
+  });
+});
+
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function(e){
+  e.preventDefault();
+  console.log('Navigation to', e.target.textContent, 'was prevented');
+});
