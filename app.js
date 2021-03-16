28f863b4553bb2594f64b6e4f74057278a942a18
Edgars Meirans <edgarsmeirans@gmail.com>
2021-03-16 16:09:01 +0200

    ok

--- app.js
+++ app.js
@@ -123,5 +123,15 @@ deleteBtn.classList.add('delete');
     list.appendChild(li);
 })
 
+//hide books
+
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  } else {
+    list.style.display = "initial";
+  }
+});
 
 
